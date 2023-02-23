028c897cad9a70a0286efab18a0c6c12bad87d84
Khrystyna Marusenko <khrystynamarusenko@MacBook-Air-Khrystyna.local>
2023-02-23 12:30:18 +0200

    added body-parser module and updated post requests

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express'); // web framework for node.js
 const morgan = require('morgan'); // logs
+const bodyParser = require('body-parser') // for parsing data
 
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//middlewares
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use('/products', productRoutes); // http://localhost:3000/products
 app.use('/orders', orderRoutes); // http://localhost:3000/orders
@@ -17,6 +21,7 @@ app.use((req, res, next) => {
     error.status = 404;
     next(error);
 })
+
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
