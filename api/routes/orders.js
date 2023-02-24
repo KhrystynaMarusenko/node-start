baa5322ce46f7ebbefd25975cf9444fd0f955e5d
Khrystyna Marusenko <khrystynamarusenko@MacBook-Air-Khrystyna.local>
2023-02-24 13:53:05 +0200

    order API

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,37 +1,134 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+
+const Order = require("../models/order");
+const Product = require("../models/product");
 
 //RESTful api
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'hedling get for /orders'
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select("-__v")
+    .exec()
+    .then((orders) => {
+      const response = {
+        count: orders.length,
+        orders: orders.map((order) => {
+          return {
+            _id: order._id,
+            product: order.product,
+            quantity: order.quantity,
+            requestAnOrder: {
+              type: "GET",
+              description: "Get order",
+              url: `http://localhost:3000/orders/${order._id}`,
+            },
+            requestProduct: {
+              type: "GET",
+              description: "Get product",
+              url: `http://localhost:3000/products/${order.product}`,
+            },
+          };
+        }),
+      };
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-})
+});
 
-router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
+router.post("/", (req, res, next) => {
+  Product.findById(req.body.productId)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({
+          message: "Product not found",
+        });
+      }
+      const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        product: req.body.productId,
         quantity: req.body.quantity,
-    }
-    res.status(201).json({
-        message: 'hedling post  for /orders',
-        order: order
+      });
+      return order.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: "Created order successfully",
+        createdOrder: {
+          quantity: result.quantity,
+          product: result.product,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: `http://localhost:3000/orders/${result._id}`,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Product not found",
+        error: err,
+      });
     });
-})
+});
 
-router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'hedling get with ID for /orders',
-        orderId: req.params.orderId,
+router.get("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
+
+  Order.findById(id)
+    .select("-__v")
+    .exec()
+    .then((order) => {
+      if (order) {
+        res.status(200).json({
+          order: order,
+          requestProduct: {
+            type: "GET",
+            description: "Get product",
+            url: `http://localhost:3000/products/${order.product}`,
+          },
+          request: {
+            type: "GET",
+            description: "Get ALL orders",
+            url: "http://localhost:3000/orders/",
+          },
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-})
+});
 
+router.delete("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
 
-router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'deleted order for /orders',
+  Order.deleteOne({ _id: id })
+    .exec()
+    .then(() => {
+      res.status(200).json({
+        message: "Order deleted",
+        request: {
+          type: "GET",
+          description: "Get ALL orders",
+          url: "http://localhost:3000/orders/",
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-})
+});
 
 module.exports = router;
